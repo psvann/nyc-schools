09b7833d72ad4649793a009df0c797172d3ca8e4
P.S. Vann <psvann@yahoo.com>
2024-03-12 13:09:40 -0400

    updates

--- app.py
+++ app.py
@@ -11,16 +11,27 @@ st.markdown("""<style> .reportview-container { top: 0px; } </style>""", unsafe_a
 
 
 df = pd.read_csv("data/full_joined_original_nyc_schools_dataset.csv")
-df_map = df[["location_name","LATITUDE","LONGITUDE","Personal Attention and Support","Student-Teacher Trust","Grades_final_text","Administrative_District_Name"]]
-df_map["Student Teacher Trust (Normalized)"] = 1 / df_map['Student-Teacher Trust'].astype('int') / 100
 
-fig = px.scatter_mapbox(df_map, 
+df_map = df[["location_name","Grades_final_text","LATITUDE","LONGITUDE","Personal Attention and Support","Student-Teacher Trust","Administrative_District_Name"]]
+
+# school_type = st.selectbox('Select School Type', 
+#                            df_map['Grades_final_text'].unique() )
+# filtered_df = df[df['Grades_final_text'] == school_type]
+
+filtered_df = df_map[df_map['Grades_final_text'].str.contains('07',na=False)]
+
+filtered_df["Student Teacher Trust (Normalized)"] = 100 - filtered_df['Student-Teacher Trust'].astype('int')
+
+filtered_df["Personal Attention and Support (Normalized)"] = 100 - filtered_df['Personal Attention and Support'].astype('int')
+
+fig = px.scatter_mapbox(filtered_df, 
                         lat="LATITUDE", 
                         lon="LONGITUDE", 
-                        color="Personal Attention and Support", 
-                        size="Student Teacher Trust (Normalized)",
+                        # color="Personal Attention and Support", 
+                        size="Personal Attention and Support (Normalized)",
                         hover_name="location_name",
-                        color_continuous_scale=px.colors.sequential.RdBu, 
+                        hover_data=["location_name","Grades_final_text","Personal Attention and Support (Normalized)","Student Teacher Trust (Normalized)"],
+                        # color_continuous_scale=px.colors.sequential.RdBu, 
                         size_max=10, 
                         zoom=10,
                         height=800)
